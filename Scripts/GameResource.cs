eee8f58fd43511677413ec993f0c7513241b226c
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-01 23:31:39 -0500

    Create GameResource.cs

---
+++ Scripts/GameResource.cs
@@ -0,0 +1,25 @@
+namespace GodotRTS;
+
+public partial class GameResource : Node2D
+{
+    [Signal] public delegate void DestroyedEventHandler();
+
+    public Area2D areaDetect;
+
+    public override void _Ready()
+    {
+        areaDetect = GetNode<Area2D>("Area2D");
+        areaDetect.BodyEntered += body =>
+        {
+            if (body is Unit unit)
+            {
+                unit.MoveToBase();
+            }
+        };
+    }
+
+    public void Destroy()
+    {
+        EmitSignal(SignalName.Destroyed);
+    }
+}
