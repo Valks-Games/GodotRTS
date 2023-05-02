7b5a68deeed3d6aa3856127c04c4f6a29be32038
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-01 23:31:43 -0500

    Update Unit.cs

--- Scripts/Unit.cs
+++ Scripts/Unit.cs
@@ -2,17 +2,17 @@ namespace GodotRTS;
 
 public partial class Unit : RigidBody2D
 {
-    private GTimer timerLogic;
+    [Signal] public delegate void DestroyedEventHandler();
+
     private Vector2 direction = Vector2.Zero;
+    private Vector2 targetPos = Vector2.Zero;
     private float speed = 25;
-    private State state = State.GatheringResources;
+    private State prevState = State.Idle;
+    private State state;
 
     public override void _Ready()
     {
-        timerLogic = new GTimer(this, Logic, 1000) {
-            Loop = true
-        };
-        timerLogic.StartMs();
+        MoveToResource();
     }
 
     public override void _PhysicsProcess(double delta)
@@ -20,39 +20,75 @@ public partial class Unit : RigidBody2D
         ApplyCentralForce(direction * speed);
     }
 
-    private void Logic()
+    public void Update()
     {
-        if (state == State.GatheringResources)
-        {
-            if (Game.Resources.Count > 0)
-            {
-                var resourcePos = Game.Resources[0].Position;
+        UpdateDirection();
+    }
 
-                if (Position.DistanceTo(resourcePos) < 100)
-                {
-                    state = State.DroppingResourcesOffAtBase;
-                }
-                else
-                {
-                    direction = (resourcePos - Position).Normalized();
-                }
-            }
-        }
-        else if (state == State.DroppingResourcesOffAtBase)
-        {
-            var basePos = Game.Base.Position;
-            direction = (basePos - Position).Normalized();
+    public void Destroy()
+    {
+        EmitSignal(SignalName.Destroyed);
+    }
 
-            if (Position.DistanceTo(basePos) < 100)
-            {
-                state = State.GatheringResources;
-            }
+    public void MoveToResource()
+    {
+        if (Game.Resources.Count == 0)
+            return;
+
+        SwitchState(State.MovingToResource);
+
+        if (prevState == State.MovingToBase)
+        {
+            Game.Team1Base.Destroyed -= StopIfTargetDestroyed;
         }
+
+        Game.Resources[0].Destroyed += StopIfTargetDestroyed;
+
+        targetPos = Game.Resources[0].Position;
+        UpdateDirection();
+    }
+
+    public void MoveToBase()
+    {
+        if (Game.Team1Base == null)
+            return;
+
+        SwitchState(State.MovingToBase);
+
+        if (prevState == State.MovingToResource)
+        {
+            Game.Resources[0].Destroyed -= StopIfTargetDestroyed;
+        }
+
+        Game.Team1Base.Destroyed += StopIfTargetDestroyed;
+
+        targetPos = Game.Team1Base.Position;
+        UpdateDirection();
+    }
+
+    private void StopIfTargetDestroyed()
+    {
+        if (state == State.MovingToResource)
+        {
+            SetPhysicsProcess(false);
+        }
+    }
+
+    private void UpdateDirection()
+    {
+        direction = (targetPos - Position).Normalized();
+    }
+
+    private void SwitchState(State state)
+    {
+        prevState = this.state;
+        this.state = state;
     }
 }
 
 public enum State
 {
-    GatheringResources,
-    DroppingResourcesOffAtBase
+    MovingToResource,
+    MovingToBase,
+    Idle
 }
