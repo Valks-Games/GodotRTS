2964da2df1548d481f9e6a19c57959422a0cf276
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-01 23:31:37 -0500

    Update Game.cs

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -2,25 +2,47 @@ namespace GodotRTS;
 
 public partial class Game : Node
 {
-    public static List<Resource> Resources { get; } = new();
-    public static Base Base { get; private set; }
+    public static List<GameResource> Resources { get; } = new();
+    public static Base Team1Base { get; private set; }
+
+    private readonly static List<Unit> units = new();
+    private static Node team1UnitsParent;
+    private GTimer timerUpdate;
+
+    public static void AddUnit(Unit unit)
+    {
+        team1UnitsParent.AddChild(unit);
+        units.Add(unit);
+    }
 
     public override void _Ready()
     {
-        GetBase();
+        timerUpdate = new GTimer(this, Update, 500) {
+            Loop = true
+        };
+        timerUpdate.StartMs();
+
+        team1UnitsParent = GetNode("Team 1/Units");
+        Team1Base = GetNode<Base>("Team 1/Base");
         GetResources();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Team1Base.CreateUnit();
+        }
     }
 
-    private void GetBase()
+    private void Update()
     {
-        Base = GetNode<Base>("Team 1/Base");
+        foreach (var unit in units)
+            unit.Update();
     }
 
     private void GetResources()
     {
         var nodes = GetNode("Environment/Resources");
 
-        foreach (var resource in nodes.GetChildren<Resource>())
+        foreach (var resource in nodes.GetChildren<GameResource>())
             Resources.Add(resource);
     }
 }
