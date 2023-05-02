e3c27f3e8ef8b14c374c5b697e3517f9d05ec673
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-01 21:56:16 -0500

    Update Unit.cs

--- Scripts/Unit.cs
+++ Scripts/Unit.cs
@@ -1,6 +1,58 @@
-using Godot;
-using System;
+namespace GodotRTS;
 
-public partial class Unit : Node
+public partial class Unit : RigidBody2D
 {
+    private GTimer timerLogic;
+    private Vector2 direction = Vector2.Zero;
+    private float speed = 25;
+    private State state = State.GatheringResources;
+
+    public override void _Ready()
+    {
+        timerLogic = new GTimer(this, Logic, 1000) {
+            Loop = true
+        };
+        timerLogic.StartMs();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        ApplyCentralForce(direction * speed);
+    }
+
+    private void Logic()
+    {
+        if (state == State.GatheringResources)
+        {
+            if (Game.Resources.Count > 0)
+            {
+                var resourcePos = Game.Resources[0].Position;
+
+                if (Position.DistanceTo(resourcePos) < 100)
+                {
+                    state = State.DroppingResourcesOffAtBase;
+                }
+                else
+                {
+                    direction = (resourcePos - Position).Normalized();
+                }
+            }
+        }
+        else if (state == State.DroppingResourcesOffAtBase)
+        {
+            var basePos = Game.Base.Position;
+            direction = (basePos - Position).Normalized();
+
+            if (Position.DistanceTo(basePos) < 100)
+            {
+                state = State.GatheringResources;
+            }
+        }
+    }
+}
+
+public enum State
+{
+    GatheringResources,
+    DroppingResourcesOffAtBase
 }
