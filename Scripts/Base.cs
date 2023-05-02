1c8db13bacf62c1a15317100a70c56fb6c86d54c
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-01 23:31:35 -0500

    Update Base.cs

--- Scripts/Base.cs
+++ Scripts/Base.cs
@@ -1,6 +1,34 @@
+using GodotUtils;
+
 namespace GodotRTS;
 
 public partial class Base : Node2D
 {
+    [Signal] public delegate void DestroyedEventHandler();
 
+    private Vector2 unitSpawnPos;
+    private Area2D areaDetect;
+
+    public override void _Ready()
+    {
+        unitSpawnPos = GetNode<Marker2D>("UnitSpawnPos").GlobalPosition;
+        areaDetect = GetNode<Area2D>("Area2D");
+        areaDetect.BodyEntered += body =>
+        {
+            if (body is Unit unit)
+                unit.MoveToResource();
+        };
+    }
+
+    public void CreateUnit()
+    {
+        var unit = (Unit)Prefabs.Unit.Instantiate();
+        unit.Position = unitSpawnPos;
+        Game.AddUnit(unit);
+    }
+
+    public void Destroy()
+    {
+        EmitSignal(SignalName.Destroyed);
+    }
 }
